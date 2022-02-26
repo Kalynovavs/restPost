2e6405410a164a12b6792214531aacc39d37910d
kalynovavs <valentinka18.12.95@gmail.com>
2022-02-27 02:23:01 +0300

    init commit

---
+++ src/test/java/ru/netology/test/MobileBankApiPostTest.java
@@ -0,0 +1,24 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class MobileBankApiPostTest {
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("test post")
+                // Выполняемые действия
+        .when()
+                .post("/post")
+                // Проверки
+        .then()
+                .statusCode(200)
+                .body("data", equalTo("test poststst"));
+    }
+}
